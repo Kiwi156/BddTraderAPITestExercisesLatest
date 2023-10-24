7cf5ea440848328ba90f651c722d3cea7bf078fc
Katie Beattie <KatieBeattie13@gmail.com>
2023-10-25 08:08:26 +1300

    My first API test.

---
+++ src/test/java/net/bddtrader/unittests/company/WhenGettingCompanyDetails.java
@@ -0,0 +1,19 @@
+package net.bddtrader.unittests.company;
+
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+public class WhenGettingCompanyDetails {
+
+    @Test
+    public void shouldReturnNameAndSector(){
+        RestAssured.get("https://bddtrader.herokuapp.com/api/stock/aapl/company")
+                .then()
+                .body("companyName", Matchers.equalTo("Apple, Inc."))
+                .body("sector", Matchers.equalTo("Electronic Technology"));
+    }
+
+
+
+}
